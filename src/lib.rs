7477f9a981cb0dd87876f978f1353504e1c8a742
Alex Elsayed <eternaleye@gmail.com>
2015-04-21 03:33:17 -0700

    Massive refactor for a more collections-like API

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
-#![feature(box_patterns)]
 #![feature(core)]
 
 use std::num::Int;
 
-use ::CritBit::{Leaf,Internal,Empty};
+pub struct CritBit<K,V>( Option<CritBitNode<K,V>> ) where K: Int + Eq;
 
-pub enum CritBit<K,V> {
+pub enum CritBitNode<K,V> where K: Int + Eq {
     Leaf ( K, V ),
-    Internal ( (Box<CritBit<K,V>>, Box<CritBit<K,V>>), u32 ),
-    Empty
+    Internal ( (Option<Box<CritBitNode<K,V>>>, Option<Box<CritBitNode<K,V>>>), u32 ),
 }
 
 #[inline(always)]
@@ -16,122 +14,110 @@ fn bit_at<T: Int + Eq>( value: &T, pos: &u32 ) -> bool {
     value.rotate_left(*pos).leading_zeros() == 0
 }
 
-impl<K: Int + Eq, V> CritBit<K, V> {
+impl<K,V> CritBit<K,V> where K: Int + Eq {
+    pub fn new() -> CritBit<K,V> {
+        CritBit( None )
+    }
+
+    pub fn clear( &mut self ) {
+        self.0 = None;
+    }
+
+    pub fn len( &self ) -> usize {
+        self.0.iter().map(CritBitNode::len).sum()
+    }
+
+    pub fn get( &self, key: &K ) -> Option<&V> {
+        match &self.0 {
+            &Some( ref node ) => node.get( key ),
+            &None => None
+        }
+    }
+
+    pub fn get_mut( &mut self, key: &K ) -> Option<&mut V> {
+        match &mut self.0 {
+            &mut Some( ref mut node ) => node.get_mut( key ),
+            &mut None => None
+        }
+    }
+
+    pub fn contains_key( &self, key: &K ) -> bool {
+        self.get( key ).is_some()
+    }
+
+    pub fn insert( &mut self, key: K, value: V ) -> Option<V> {
+        match &mut self.0 {
+            &mut Some( ref mut node ) => node.insert( key, value ),
+            x => { std::mem::replace( x, Some( CritBitNode::Leaf( key, value ) ) ); None }
+        }
+    }
+}
+
+impl<K: Int + Eq, V> CritBitNode<K, V> {
     fn len( &self ) -> usize {
         match *self {
-            Empty => 0,
-            Leaf ( .. ) => 1,
-            Internal( ( ref left, ref right ), _ ) => {
-                left.len() + right.len()
+            CritBitNode::Leaf ( .. ) => 1,
+            CritBitNode::Internal( ( ref left, ref right ), _ ) => {
+                left.iter().chain(right.iter()).map(|x| x.len()).sum()
             }
         }
     }
 
-    fn find<'a>( &'a self, key: &K ) -> Option<&'a V> {
+    fn get( &self, key: &K ) -> Option<&V> {
         match *self {
-            Leaf ( ref k, ref v ) if *k == *key =>
+            CritBitNode::Leaf ( ref k, ref v ) if *k == *key =>
                 Some( v ),
-            Internal ( ( ref left, _ ), ref crit ) if ! bit_at( key, crit ) =>
-                left.find( key ),
-            Internal ( ( _, ref right ), ref crit ) if   bit_at( key, crit ) =>
-                right.find( key ),
+            CritBitNode::Internal ( ( Some( ref left ), _ ), ref crit ) if ! bit_at( key, crit ) =>
+                left.get( key ),
+            CritBitNode::Internal ( ( _, Some( ref right ) ), ref crit ) if   bit_at( key, crit ) =>
+                right.get( key ),
             _ => None
         }
     }
 
-    fn contains_key( &self, key: &K ) -> bool {
-        match self.find( key ) {
-            Some( _ ) => true,
-            None      => false
-        }
-    }
-
-    fn clear( &mut self ) {
-        *self = Empty
-    }
-
-    fn find_mut<'a>( &'a mut self, key: &K ) -> Option<&'a mut V> {
+    fn get_mut( &mut self, key: &K ) -> Option<&mut V> {
         match *self {
-            Leaf ( ref k, ref mut v ) if *k == *key =>
+            CritBitNode::Leaf ( ref k, ref mut v ) if *k == *key =>
                 Some( v ),
-            Internal ( ref mut children, ref crit ) if ! bit_at( key, crit ) =>
-                children.0.find_mut( key ),
-            Internal ( ref mut children, ref crit ) if   bit_at( key, crit ) =>
-                children.1.find_mut( key ),
+            CritBitNode::Internal ( ( Some( ref mut kid ), _ ), ref crit ) if ! bit_at( key, crit ) =>
+                kid.get_mut( key ),
+            CritBitNode::Internal ( ( _, Some( ref mut kid ) ), ref crit ) if   bit_at( key, crit ) =>
+                kid.get_mut( key ),
             _ => None
         }
     }
 
-    fn pop( &mut self, key: &K ) -> Option<V> {
-        let mut val = std::mem::replace( self, Empty );
-        let ret = match val {
-            Internal ( ref mut children, ref crit ) if ! bit_at( key, crit ) =>
-                children.0.pop( key ),
-            Internal ( ref mut children, ref crit ) if   bit_at( key, crit ) =>
-                children.1.pop( key ),
-            _ => None
-        };
-
-        match val {
-            Leaf ( k, v ) => {
-                if k == *key {
-                    Some ( v )
-                } else {
-                    std::mem::replace( self, Leaf ( k, v ) );
-                    None
-                }
+    fn insert( &mut self, key: K, value: V ) -> Option<V> {
+        match *self {
+            CritBitNode::Leaf ( ref k, ref mut v ) if *k == key => {
+                Some( std::mem::replace( v, value ) )
             }
-            Internal ( ( box Empty, kid ), _ ) => {
-                std::mem::replace( self, *kid );
-                ret
-            },
-            Internal ( ( kid, box Empty ), _ ) => {
-                std::mem::replace( self, *kid );
-                ret
-            },
-            _ => {
-                std::mem::replace( self, val );
-                ret
-            }
-        }
-    }
-
-    fn swap( &mut self, key: K, value: V ) -> Option<V> {
-        let val = std::mem::replace( self, Empty );
-        match val {
-            Leaf ( k, v ) => {
-                let crit = ( k ^ key ).leading_zeros();
-                let bit = bit_at( &key, &crit );
-                if k == key {
-                    std::mem::replace( self, Leaf ( key, value ) );
-                    Some( v )
-                } else if bit {
-                    std::mem::replace( self, Internal (
-                        ( Box::new( Leaf ( k, v ) ), Box::new( Leaf ( key, value ) ) ), crit
-                    ) );
-                    None
+            CritBitNode::Leaf ( .. ) => {
+                if let CritBitNode::Leaf ( k, v ) = std::mem::replace( self, CritBitNode::Internal( ( None, None ), 0 ) ) {
+                    let crit = (k ^ key).leading_zeros();
+                    std::mem::replace(self, CritBitNode::Internal (
+                        if k < key {
+                            (
+                                Some( Box::new( CritBitNode::Leaf ( k, v ) ) ),
+                                Some( Box::new( CritBitNode::Leaf ( key, value ) ) ),
+                            )
+                        } else {
+                            (
+                                Some( Box::new( CritBitNode::Leaf ( key, value ) ) ),
+                                Some( Box::new( CritBitNode::Leaf ( k, v ) ) ),
+                            )
+                        }, crit
+                    ));
                 } else {
-                    std::mem::replace( self, Internal (
-                        ( Box::new( Leaf ( key, value ) ), Box::new( Leaf ( k, v ) ) ), crit
-                    ) );
-                    None
+                    unreachable!()
                 }
-            },
-            Internal ( .. ) => {
-                std::mem::replace( self, val );
-                match *self {
-                    Internal ( ref mut children, ref crit ) if ! bit_at( &key, crit ) =>
-                        children.0.swap( key, value ),
-                    Internal ( ref mut children, ref crit ) if   bit_at( &key, crit ) =>
-                        children.1.swap( key, value ),
-                    _ => None
-                }
-
-            },
-            Empty => {
-                std::mem::replace( self, Leaf ( key, value ) );
                 None
-            }
+            },
+            CritBitNode::Internal ( ( Some( ref mut kid ), _ ), ref crit ) if ! bit_at( &key, crit ) =>
+                kid.insert( key, value ),
+            CritBitNode::Internal ( ( _, Some( ref mut kid ) ), ref crit ) if   bit_at( &key, crit ) =>
+                kid.insert( key, value ),
+            _ => unreachable!()
         }
     }
 }
@@ -146,209 +132,81 @@ fn verify_bit_at() {
 
 #[test]
 fn empty_len() {
-    let t : CritBit<u8,()> = Empty;
+    let t : CritBit<u8,()> = CritBit::new();
     assert_eq!( t.len(), 0 );
 }
 
 #[test]
 fn empty_contains_key() {
-    let t : CritBit<u8,()> = Empty;
+    let t : CritBit<u8,()> = CritBit::new();
     assert_eq!( t.contains_key( &0u8 ), false );
     assert_eq!( t.contains_key( &128u8 ), false );
     assert_eq!( t.contains_key( &255u8 ), false );
 }
 
 #[test]
-fn empty_find() {
-    let t : CritBit<u8,()> = Empty;
-    assert_eq!( t.find( &0u8 ), None );
-    assert_eq!( t.find( &128u8 ), None );
-    assert_eq!( t.find( &255u8 ), None );
+fn empty_get() {
+    let t : CritBit<u8,()> = CritBit::new();
+    assert_eq!( t.get( &0u8 ), None );
+    assert_eq!( t.get( &128u8 ), None );
+    assert_eq!( t.get( &255u8 ), None );
 }
 
 #[test]
-fn leaf_len() {
-    let t : CritBit<u8,()> = Leaf ( 0u8, () );
+fn empty_get_mut() {
+    let mut t : CritBit<u8,()> = CritBit::new();
+    assert!( t.get_mut( &0u8 ).is_none() );
+    assert!( t.get_mut( &128u8 ).is_none() );
+    assert!( t.get_mut( &255u8 ).is_none() );
+}
+
+#[test]
+fn insert_len() {
+    let mut t : CritBit<u8,()> = CritBit::new();
+    assert_eq!( t.len(), 0 );
+
+    t.insert( 0u8, () );
     assert_eq!( t.len(), 1 )
 }
 
 #[test]
-fn leaf_contains_key() {
-    let t : CritBit<u8,()> = Leaf ( 0u8, () );
+fn insert_contains_key() {
+    let mut t : CritBit<u8,()> = CritBit::new();
+    assert_eq!( t.contains_key( &0u8 ), false );
+
+    t.insert( 0u8, () );
     assert_eq!( t.contains_key( &0u8 ), true );
-    assert_eq!( t.contains_key( &128u8 ), false );
-    assert_eq!( t.contains_key( &255u8 ), false );
 }
 
 #[test]
-fn leaf_find() {
-    let t : CritBit<u8,u8> = Leaf ( 0u8, 1u8 );
-    let val = 1u8;
-    assert_eq!( t.find( &0u8 ), Some ( &val ) );
-    assert_eq!( t.find( &128u8 ), None );
-    assert_eq!( t.find( &255u8 ), None );
+fn insert_get() {
+    let mut t : CritBit<u8,u8> = CritBit::new();
+    assert_eq!( t.get( &0u8 ), None );
+
+    t.insert( 0u8, 1u8 );
+    assert_eq!( t.get( &0u8 ), Some ( &1u8 ) );
 }
 
 #[test]
-fn internal_len() {
-    let t : CritBit<u8,()> = Internal (
-        ( Box::new( Leaf ( 0u8, () ) ), Box::new( Leaf ( 128u8, () ) ) ), 0u32
-    );
-    assert_eq!( t.len(), 2 );
+fn insert_get_mut() {
+    let mut t : CritBit<u8,u8> = CritBit::new();
+    assert_eq!( t.get( &0u8 ), None );
+
+    t.insert( 0u8, 1u8 );
+    assert_eq!( t.get( &0u8 ), Some ( &1u8 ) );
+
+    *t.get_mut( &0u8 ).unwrap() = 2u8;
+    assert_eq!( t.get( &0u8 ), Some ( &2u8 ) );
 }
 
 #[test]
-fn internal_contains_key() {
-    let t : CritBit<u8,()> = Internal (
-        ( Box::new( Leaf ( 0u8, () ) ), Box::new( Leaf ( 128u8, () ) ) ), 0u32
-    );
-    assert_eq!( t.contains_key( &0u8 ), true );
-    assert_eq!( t.contains_key( &128u8 ), true );
-    assert_eq!( t.contains_key( &255u8 ), false );
-}
+fn insert_insert() {
+    let mut t : CritBit<u8,u8> = CritBit::new();
+    assert_eq!( t.get( &0u8 ), None );
 
-#[test]
-fn internal_find() {
-    let t : CritBit<u8,u8> = Internal (
-        ( Box::new( Leaf ( 0u8, 1u8 ) ), Box::new( Leaf ( 128u8, 1u8 ) ) ), 0u32
-    );
-    let val = 1u8;
-    assert_eq!( t.find( &0u8 ), Some ( &val ) );
-    assert_eq!( t.find( &128u8 ), Some ( &val ) );
-    assert_eq!( t.find( &255u8 ), None );
-}
+    t.insert( 0u8, 1u8 );
+    assert_eq!( t.get( &0u8 ), Some ( &1u8 ) );
 
-#[test]
-fn leaf_clear() {
-    let mut t : CritBit<u8,()> = Leaf ( 0u8, () );
-    assert_eq!( t.len(), 1 );
-    t.clear();
-    assert_eq!( t.len(), 0 );
-}
-
-#[test]
-fn internal_clear() {
-    let mut t : CritBit<u8,()> = Internal (
-        ( Box::new( Leaf ( 0u8, () ) ), Box::new( Leaf ( 128u8, () ) ) ), 0u32
-    );
-    assert_eq!( t.len(), 2 );
-    t.clear();
-    assert_eq!( t.len(), 0 );
-}
-
-#[test]
-fn empty_find_mut() {
-    let mut t : CritBit<u8,()> = Empty;
-    assert!( t.find_mut( &0u8 ).is_none() );
-    assert!( t.find_mut( &128u8 ).is_none() );
-    assert!( t.find_mut( &255u8 ).is_none() );
-}
-
-#[test]
-fn leaf_find_mut() {
-    let mut t : CritBit<u8,u8> = Leaf ( 0u8, 1u8 );
-    let val = 7u8;
-    {
-        let x = t.find_mut( &0u8 );
-        assert!( x.is_some() );
-        let y = x.unwrap();
-        assert_eq!( *y, 1u8 );
-        *y = 7u8;
-    }
-
-    assert_eq!( t.find( &0u8 ), Some( &val ) );
-    assert!( t.find_mut( &128u8 ).is_none() );
-    assert!( t.find_mut( &255u8 ).is_none() );
-}
-
-#[test]
-fn internal_find_mut() {
-    let mut t : CritBit<u8,u8> = Internal (
-        ( Box::new( Leaf ( 0u8, 1u8 ) ), Box::new( Leaf ( 128u8, 1u8 ) ) ), 0u32
-    );
-    let val = 7u8;
-    {
-        let x = t.find_mut( &0u8 );
-        assert!( x.is_some() );
-        let y = x.unwrap();
-        assert_eq!( *y, 1u8 );
-        *y = 7u8;
-    }
-
-    assert_eq!( t.find( &0u8 ), Some ( &val ) );
-    assert!( t.find_mut( &128u8 ).is_some() );
-    assert!( t.find_mut( &255u8 ).is_none() );
-}
-
-#[test]
-fn empty_swap() {
-    let mut t : CritBit<u8,u8> = Empty;
-    let val = 1u8;
-    assert_eq!( t.swap( 0u8, 1u8 ), None );
-    assert_eq!( t.find( &0u8 ), Some ( &val ) );
-}
-
-#[test]
-fn leaf_swap_exists() {
-    let mut t : CritBit<u8,u8> = Leaf ( 0u8, 1u8 );
-    let val = 7u8;
-    assert_eq!( t.swap( 0u8, 7u8 ), Some ( 1u8 ) );
-    assert_eq!( t.find( &0u8 ), Some ( &val ) );
-}
-
-#[test]
-fn leaf_swap_new() {
-    let mut t : CritBit<u8,u8> = Leaf ( 0u8, 1u8 );
-    let oldval = 1u8;
-    let val = 7u8;
-    assert_eq!( t.swap( 128u8, 7u8 ), None );
-    assert_eq!( t.len(), 2 );
-    assert_eq!( t.find( &0u8 ), Some ( &oldval ) );
-    assert_eq!( t.find( &128u8 ), Some ( &val ) );
-}
-
-#[test]
-fn internal_swap_new() {
-    let mut t : CritBit<u8,u8> = Internal (
-        ( Box::new( Leaf ( 0u8, 1u8 ) ), Box::new( Leaf ( 128u8, 1u8 ) ) ), 0u32
-    );
-    let oldval = 1u8;
-    let val = 7u8;
-    assert_eq!( t.swap( 255u8, 7u8 ), None );
-    assert_eq!( t.len(), 3 );
-    assert_eq!( t.find( &0u8 ), Some ( &oldval ) );
-    assert_eq!( t.find( &128u8 ), Some ( &oldval ) );
-    assert_eq!( t.find( &255u8 ), Some ( &val ) );
-}
-
-#[test]
-fn internal_swap_exists() {
-    let mut t : CritBit<u8,u8> = Internal (
-        ( Box::new( Leaf ( 0u8, 1u8 ) ), Box::new( Leaf ( 128u8, 1u8 ) ) ), 0u32
-    );
-    let val = 7u8;
-    assert_eq!( t.swap( 0u8, 7u8 ), Some ( 1u8 ) );
-    assert_eq!( t.find( &0u8 ), Some ( &val ) );
-}
-
-#[test]
-fn empty_pop() {
-    let mut t : CritBit<u8,()> = Empty;
-    assert_eq!( t.pop( &0u8 ), None );
-}
-
-#[test]
-fn leaf_pop() {
-    let mut t : CritBit<u8,()> = Leaf ( 0u8, () );
-    assert_eq!( t.pop( &0u8 ), Some ( () ) );
-    assert_eq!( t.len(), 0 );
-}
-
-#[test]
-fn internal_pop() {
-    let mut t : CritBit<u8,()> = Internal (
-        ( Box::new( Leaf ( 0u8, () ) ), Box::new( Leaf ( 128u8, () ) ) ), 0u32
-    );
-    assert_eq!( t.pop( &0u8 ), Some ( () ) );
-    assert_eq!( t.len(), 1 );
+    assert_eq!( t.insert( 0u8, 2u8 ), Some ( 1u8 ) );
+    assert_eq!( t.get( &0u8 ), Some ( &2u8 ) );
 }
